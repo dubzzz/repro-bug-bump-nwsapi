29ba7250c88ad35fdd9735f6e64fb4048273279b
Nicolas DUBIEN <github@dubien.org>
2025-01-07 09:24:27 +0000

    still not failing

--- Main.spec.tsx
+++ Main.spec.tsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { test } from "vitest";
 import React from "react";
 
 const Name = styled.span``;
 const Compo = styled.div`
   &:focus-within:not(:has(${Name}:focus)) {
+    border: 1px;
   }
 `;
 
-test("attempt", () => {
-  render(<Compo />);
+test("attempt", async () => {
+  render(
+    <Compo data-testid="plip">
+      <Name data-testid="plop" />
+    </Compo>
+  );
+  await userEvent.click(await screen.findByTestId("plip"));
+  await userEvent.click(await screen.findByTestId("plop"));
 });
